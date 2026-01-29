f44dabdcb2631c7c19bb4b524c0fb5e7ccee510a
Sönke Hahn <soenkehahn@gmail.com>
2026-01-29 16:39:41 -0500

    Allow filtering by process arguments (#35)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use crate::process::ProcessWatcher;
-use crate::regex::Regex;
 use crate::treetop_app::TreetopApp;
 use clap::Parser;
 use std::error::Error;
@@ -16,16 +15,20 @@ type R<A> = Result<A, Box<dyn Error>>;
 
 #[derive(Parser, Debug)]
 struct Args {
-    #[arg(help = "search pattern for filtering the process tree")]
+    /// Search pattern for filtering the process tree
     pattern: Option<String>,
+
+    #[arg(long)]
+    /// Prevents treetop from hiding itself
+    ///
+    /// By default treetop will hide itself (i.e. its own process) if and only if matched on
+    /// process arguments. Otherwise treetop would often show itself when passing a search pattern
+    /// as an argument. This is usually not useful. This flag makes sure treetop always shows
+    /// itself when matched.
+    dont_hide_self: bool,
 }
 
 fn main() -> R<()> {
     let args = Args::parse();
-    TreetopApp::run(TreetopApp::new(
-        ProcessWatcher::new(System::new()),
-        args.pattern
-            .map(|pattern| ::regex::Regex::new(&pattern).map(crate::Regex::new))
-            .transpose()?,
-    )?)
+    TreetopApp::run(TreetopApp::new(ProcessWatcher::new(System::new()), args)?)
 }
