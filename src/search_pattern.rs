19a1f8585583f99b45b58d2b44d382b845f5520b
Sönke Hahn <soenkehahn@gmail.com>
2026-02-05 14:37:41 -0500

    Try with SearchPattern::Empty

--- src/search_pattern.rs
+++ src/search_pattern.rs
@@ -2,6 +2,7 @@ use regex::Regex;
 
 #[derive(Debug)]
 pub(crate) enum SearchPattern {
+    Empty,
     Regex { regex: regex::Regex },
     Invalid { regex: String },
 }
@@ -12,6 +13,9 @@ impl SearchPattern {
     }
 
     pub(crate) fn from_string(regex: &str) -> SearchPattern {
+        if regex.is_empty() {
+            return SearchPattern::Empty;
+        }
         match Regex::new(regex) {
             Ok(regex) => SearchPattern::Regex { regex },
             Err(_) => SearchPattern::Invalid {
@@ -22,6 +26,7 @@ impl SearchPattern {
 
     pub(crate) fn is_match(&self, s: &str) -> bool {
         match self {
+            SearchPattern::Empty => true,
             SearchPattern::Regex { regex } => regex.is_match(s),
             SearchPattern::Invalid { .. } => false,
         }
@@ -29,6 +34,7 @@ impl SearchPattern {
 
     pub(crate) fn as_str(&self) -> &str {
         match self {
+            SearchPattern::Empty => "",
             SearchPattern::Regex { regex } => regex.as_str(),
             SearchPattern::Invalid { regex } => regex.as_str(),
         }
