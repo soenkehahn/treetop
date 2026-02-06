0b490c0c98634ea59be3b804031e1f797a06d25a
Alex David <git@alexd.me>
2026-02-06 14:03:01 -0800

    Highlight process name matches

--- src/utils.rs
+++ src/utils.rs
@@ -12,6 +12,7 @@ pub(crate) mod test {
             _ => "some-process",
         }
     }
+
     pub(crate) fn underline(s: &str) -> String {
         let mut result = String::new();
         for char in s.chars() {
@@ -19,5 +20,31 @@ pub(crate) mod test {
             result.push('\u{35f}');
         }
         return result;
+    }
+
+    pub(crate) fn bold(s: &str) -> String {
+        let mut result = String::new();
+        for char in s.chars() {
+            result.push(char);
+            result.push('\u{333}');
+        }
+        return result;
+
+        // OR:
+
+        // let mut result = String::new();
+        // for mut char in s.chars() {
+        //     if 'A' <= char && char <= 'Z' {
+        //         const BOLD_CAPITAL_A: u32 = 0x1D538;
+        //         char = char::from_u32(BOLD_CAPITAL_A + char as u32 - 'A' as u32).unwrap();
+        //     }
+        //     if 'a' <= char && char <= 'z' {
+        //         const BOLD_SMALL_A: u32 = 0x1D552;
+        //         char = char::from_u32(BOLD_SMALL_A + char as u32 - 'a' as u32).unwrap();
+        //     }
+        //     result.push(char);
+        //     result.push('\u{35f}');
+        // }
+        // return result;
     }
 }
